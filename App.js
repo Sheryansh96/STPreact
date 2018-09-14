ef71d746aacfa81409ceb10870de1b5dfe2baebb
shreyansh <shreyanshrocknroll@gmail.com>
2018-09-14 11:29:00 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Main from './app/Components/Main'
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main></Main>
+    );
+  }
+}
